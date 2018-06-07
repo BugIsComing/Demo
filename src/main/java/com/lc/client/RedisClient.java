92d6ed9d6511b86a146a22cbf07825b14ac7e504
刘诚 <LC513901@163.com>
2018-06-07 16:10:57 +0800

    2018年6月7日16:10:50
    继续学习

---
+++ src/main/java/com/lc/client/RedisClient.java
@@ -0,0 +1,92 @@
+package com.lc.client;
+
+import redis.clients.jedis.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class RedisClient {
+    private Jedis jedis;//非切片额客户端连接
+    private JedisPool jedisPool;//非切片连接池
+    private ShardedJedis shardedJedis;//切片额客户端连接
+    private ShardedJedisPool shardedJedisPool;//切片连接池
+
+    public RedisClient()
+    {
+        initialPool();
+        initialShardedPool();
+        shardedJedis = shardedJedisPool.getResource();
+        jedis = jedisPool.getResource();
+    }
+
+    /**
+     * 初始化非切片池
+     */
+    private void initialPool()
+    {
+        // 池基本配置
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxTotal(20);
+        config.setMaxIdle(5);
+        config.setMaxWaitMillis(1000L);
+        config.setTestOnBorrow(false);
+
+        jedisPool = new JedisPool(config,"127.0.0.1",6379);
+    }
+
+    /**
+     * 初始化切片池
+     */
+    private void initialShardedPool()
+    {
+        // 池基本配置
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxTotal(20);
+        config.setMaxIdle(5);
+        config.setMaxWaitMillis(1000L);
+        config.setTestOnBorrow(false);
+        // slave链接
+        List<JedisShardInfo> shards = new ArrayList<JedisShardInfo>();
+        shards.add(new JedisShardInfo("127.0.0.1", 6379, "master"));
+        // 构造池
+        shardedJedisPool = new ShardedJedisPool(config, shards);
+    }
+
+    public Jedis getJedis() {
+        return jedis;
+    }
+
+    public void setJedis(Jedis jedis) {
+        this.jedis = jedis;
+    }
+
+    public JedisPool getJedisPool() {
+        return jedisPool;
+    }
+
+    public void setJedisPool(JedisPool jedisPool) {
+        this.jedisPool = jedisPool;
+    }
+
+    public ShardedJedis getShardedJedis() {
+        return shardedJedis;
+    }
+
+    public void setShardedJedis(ShardedJedis shardedJedis) {
+        this.shardedJedis = shardedJedis;
+    }
+
+    public ShardedJedisPool getShardedJedisPool() {
+        return shardedJedisPool;
+    }
+
+    public void setShardedJedisPool(ShardedJedisPool shardedJedisPool) {
+        this.shardedJedisPool = shardedJedisPool;
+    }
+
+    public static void main(String[]args){
+        RedisClient redisClient = new RedisClient();
+        Map<String, String> map = redisClient.getJedis().hgetAll("");
+    }
+}

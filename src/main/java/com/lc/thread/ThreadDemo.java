e37b4890a20963a407e642f2922ec8f14f97be3a
刘诚 <LC513901@163.com>
2018-10-24 22:44:40 +0800

    2018年10月24日22:44:25
    学习创建线程的方式

--- src/main/java/com/lc/thread/ThreadDemo.java
+++ src/main/java/com/lc/thread/ThreadDemo.java
@@ -6,4 +6,31 @@ package com.lc.thread;
  * @date 2018-10-22 22:04:40
  **/
 public class ThreadDemo {
+    public static void main(String[] args) {
+        Runnable jobRun = new JobRun();
+        new Thread(jobRun).start();
+
+        Thread jobThread = new JobThread();
+        jobThread.start();
+    }
+}
+
+/**
+ * 通过实现Runnable实行线程的创建
+ */
+class JobRun implements Runnable{
+    @Override
+    public void run(){
+        System.out.println("This thread is created by implementing a Runnable interface");
+    }
+}
+
+/**
+ * 通过继承Thread类创建线程
+ */
+class JobThread extends Thread{
+    @Override
+    public void run(){
+        System.out.println("This thread is created by extending a Thread class");
+    }
 }

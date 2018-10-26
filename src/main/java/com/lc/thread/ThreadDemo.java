232ad35c2858d26adfe008d0a958501dcfae55b9
刘诚 <LC513901@163.com>
2018-10-26 19:02:59 +0800

    2018年10月26日19:02:55
    继续

--- src/main/java/com/lc/thread/ThreadDemo.java
+++ src/main/java/com/lc/thread/ThreadDemo.java
@@ -1,5 +1,11 @@
 package com.lc.thread;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+import static java.lang.Thread.sleep;
+
 /**
  * @author lc
  * @desc 记录学习线程有关的demo
@@ -12,15 +18,28 @@ public class ThreadDemo {
 
         Thread jobThread = new JobThread();
         jobThread.start();
+        /**
+         * 这种方式方便父线程获取子线程的执行结果
+         */
+        JobCallable jobCallable = new JobCallable();
+        FutureTask<Integer> ft = new FutureTask<>(jobCallable);
+        new Thread(ft).start();
+        try {
+            System.out.println(ft.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 }
 
 /**
  * 通过实现Runnable实行线程的创建
  */
-class JobRun implements Runnable{
+class JobRun implements Runnable {
     @Override
-    public void run(){
+    public void run() {
         System.out.println("This thread is created by implementing a Runnable interface");
     }
 }
@@ -28,9 +47,24 @@ class JobRun implements Runnable{
 /**
  * 通过继承Thread类创建线程
  */
-class JobThread extends Thread{
+class JobThread extends Thread {
     @Override
-    public void run(){
+    public void run() {
         System.out.println("This thread is created by extending a Thread class");
+    }
+}
+
+/**
+ * 通过实现Callable接口创建线程
+ */
+class JobCallable implements Callable<Integer> {
+    @Override
+    public Integer call() throws Exception {
+        int sum = 0;
+        for (int i = 0; i <= 100000; i++) {
+            sum += i;
+        }
+        sleep(2000);
+        return sum;
     }
 }

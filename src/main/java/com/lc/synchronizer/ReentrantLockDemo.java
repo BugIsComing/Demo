4371ea83bb5098919d2027c19c07b7bcc16ef5af
刘诚 <LC513901@163.com>
2018-11-28 21:14:59 +0800

    2018年11月28日21:14:44
    学习ReentrantLock

---
+++ src/main/java/com/lc/synchronizer/ReentrantLockDemo.java
@@ -0,0 +1,73 @@
+package com.lc.synchronizer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author lc
+ * @desc 学习ReentrantLock
+ * @date 2018-11-27 21:34:49
+ **/
+public class ReentrantLockDemo {
+    private Lock lock = new ReentrantLock();
+
+    private void workOn() {
+        System.out.println(Thread.currentThread().getName() + ":上班!");
+    }
+
+    private void workOff() {
+        System.out.println(Thread.currentThread().getName() + ":下班");
+    }
+
+
+    public void work() {
+        try {
+            lock.lock();
+            workOn();
+            System.out.println(Thread.currentThread().getName()
+                    + "工作中!!!!");
+            Thread.sleep(100);
+            workOff();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        ReentrantLockDemo lockDemo = new ReentrantLockDemo();
+
+        int i = 0;
+        List<Thread> list = new ArrayList<>(30);
+        do {
+            Thread a = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    lockDemo.work();
+                }
+            }, "小A_" + i);
+
+            Thread b = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    lockDemo.work();
+                }
+            }, "小B_" + i);
+
+
+            list.add(a);
+            list.add(b);
+        } while (i++ < 10);
+        list.parallelStream().forEach(Thread::start);
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("main over!");
+    }
+}
